6f32e7ef5829a2b933ce664e425cd66630e3fb93
Puja Pandey <76145468+puja123574@users.noreply.github.com>
2021-06-28 00:37:28 +0530

    Add files via upload

---
+++ Aa1.java
@@ -0,0 +1,22 @@
+
+import java .util.ArrayList;
+class Aa1 
+{
+	public static void main(String[] args) 
+	{
+		ArrayList obj1=new ArrayList();
+		obj1.add(10);
+		obj1.add(20);
+		obj1.add(30);
+		obj1.add(40);
+		ArrayList obj2=new ArrayList();
+obj2.add(50);
+obj2.add(20);
+obj2.add(40);
+obj2.add(-30);
+
+obj2.add all(obj1);
+		System.out.println("obj1");
+		System.out.println("obj2");
+	}
+}
